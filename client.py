c2c9605dabad63f6f2dc617b90a4395ccf7ef53f
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-04-27 18:37:38 -0700

    Update client.py

--- client.py
+++ client.py
@@ -8,15 +8,28 @@ import os
 
 tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#SOCK_STREAM establishes TCP protocol
 udpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#SOCK_DGRAM establishes UDP protocol
-host = socket.gethostname()
+host = socket.gethostbyname('localhost')
 port = 12345
 address = (host, port)
 
 def startDataChannel(contents):
-	udpClient.sendto(contents.encode('ascii'), address)
-	print("[SERVER RESPONSE]: " + udpClient.recvfrom(1024)[0].decode('ascii'))
+	established = True
+	size = 0
+	while established:
+		udpClient.sendto(contents.encode('ascii'), address)
+		size = int(udpClient.recvfrom(1024)[0].decode('ascii'))
+		b_arr = bytearray(contents.encode('ascii'))
+		print("[SERVER RESPONSE]: {}".format(size))
+		i = 0
+		while i < size:
+			b_arr.pop(0)
+			i = i + 1
+		contents = b_arr.decode('ascii')
+		if size == 0:
+			established = False
 	closeConnection = '@'
 	udpClient.sendto(closeConnection.encode('ascii'), address)
+
 
 def startControlChannel(fileName, fileSize):
 	tcpClient.connect(address)
@@ -37,5 +50,5 @@ if __name__ == "__main__":
 	file = open(fileName, 'r')#open for reading
 	contents = file.read()
 	fileSize = len(contents)
-	startControlChannel(fileName, str(fileSize))
+	#startControlChannel(fileName, str(fileSize))
 	startDataChannel(contents)
